3a22561efc608ae09c13e3874695a66ea5657fa5
raymond.hung <raymond.hung>
2016-06-14 15:43:34 +0800

    index layout fixed

--- eggshell_v01/js/index.js
+++ eggshell_v01/js/index.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
+
+    $(".flexslider").flexslider({});
+    
     $('#index-search > button').click(function() {
         window.location.href = 'map.html?site=' + $('#index-search > select').val();
+    });
+
+    $('#index-search > button').click(function() {
+        window.location.href = 'map.html?site=' + $('#index-search > select').val();
+    });
+
+    $('#expansion').click(function() {
+        $('.sub-menu').css('display', 'block');
     });
 
 });
